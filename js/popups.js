c6ae5a88f17d608abb79abfdd576c964b55af70a
annapinkas <anna.pinkas@gmail.com>
2023-12-27 12:57:05 -0500

    Add files via upload

---
+++ js/popups.js
@@ -0,0 +1,415 @@
+$(document).ready(function() {
+        		/* bathroom pop ups*/
+            $('.popup').magnificPopup({
+          		type: 'image',
+          		closeOnContentClick: true,
+         		 mainClass: 'mfp-img-mobile',
+          		image: {
+            		verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bathroom_vanity.html">Open original eBay listing</a>';
+                }
+              }
+        	});
+            $('.popup_bath_chair').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bathroom_chair.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+            $('.popup_bath_hamper').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bathroom_hamper.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+             $('.popup_bath_screen').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bathroom_screen.html" >Open original eBay listing</a>';
+                }
+              }
+          }); 
+          $('.popup_bath_vanity').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bathroom_vanity.html" >Open original eBay listing</a>';
+                }
+              }
+          });  
+          $('.popup_bath_rug').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bathroom_rug.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          /* bedroom pop ups*/
+          $('.popup_bed_bed').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bedroom_bed.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_bed_rug').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bedroom_rug.html" >Open original eBay listing</a>';
+                }
+              }
+          }); 
+          $('.popup_bed_nightstand').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bedroom_nightstand.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_bed_desk').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bedroom_desk.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_bed_deskLamp').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bedroom_deskLamp.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_bed_readLamp').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/bedroom_lamp.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          /* entryway pop ups*/
+          $('.popup_entry_console').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/entryway_cabinet.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_entry_mirror').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/entryway_mirror.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_entry_chair').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/entryway_chair.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_entry_rug').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/entryway_rug.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_entry_lamp').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/entryway_floorLamp.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          /* livingRoom1 pop ups*/
+          $('.popup_lv1_table').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_table.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_couch').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_couch.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_tv').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_tv.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_chair').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_chair.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_mag').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_magazine.html" >Open original eBay listing</a>';
+                }
+              }
+          }); 
+           $('.popup_lv1_console').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_console.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_frame').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_frame.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_lamp').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_lamp.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv1_rug').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom1_rug.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          /* diningRoom pop ups*/
+          $('.popup_dining_set').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/diningRoom_set.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_dining_bookshelf').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/diningRoom_bookshelf.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_dining_frame').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/diningRoom_frame.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_dining_diptych').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/diningRoom_diptych.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_dining_lamp').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/diningRoom_lamp.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          /* livingRoom 2 pop ups*/
+          $('.popup_lv2_couch').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom2_couch.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv2_cabinet').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom2_cabinet.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv2_table').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom2_table.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv2_chairs').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom2_chairs.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv2_lamp').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom2_lamp.html" >Open original eBay listing</a>';
+                }
+              }
+          });
+          $('.popup_lv2_clock').magnificPopup({
+              type: 'image',
+              closeOnContentClick: true,
+             mainClass: 'mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                  return item.el.attr('title') + '</br>' + '<a class="image-source-link" href="ebay/livingRoom2_clock.html" >Open original eBay listing</a>';
+                }
+              }
+          });            
+          });
